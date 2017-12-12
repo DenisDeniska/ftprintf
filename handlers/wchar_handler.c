804224591dbb0cd3092cc402da93fd1a5e9b287c
Denys DENKIN <ddenkin@e2r7p7.unit.ua>
2017-12-12 10:56:42 +0200

    asd

--- handlers/wchar_handler.c
+++ handlers/wchar_handler.c
@@ -6,7 +6,7 @@
 /*   By: ddenkin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/07 19:53:37 by ddenkin           #+#    #+#             */
-/*   Updated: 2017/12/12 10:52:32 by ddenkin          ###   ########.fr       */
+/*   Updated: 2017/12/12 10:56:13 by ddenkin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ int			wchar_handler(va_list *va, t_form *form)
 
 	ch = va_arg(*va, int);
 	(void)form;
-	return (ft_putchar(ch));
+	return (ft_putchar((char)ch));
 }
