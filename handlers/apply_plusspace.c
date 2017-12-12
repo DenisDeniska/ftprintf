01fc0e23d9633f0c54d513822e1789c1b3c94b75
Denys DENKIN <ddenkin@e2r7p7.unit.ua>
2017-12-12 11:22:01 +0200

    ?

--- handlers/apply_plusspace.c
+++ handlers/apply_plusspace.c
@@ -6,7 +6,7 @@
 /*   By: ddenkin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 16:27:14 by ddenkin           #+#    #+#             */
-/*   Updated: 2017/12/12 11:21:16 by ddenkin          ###   ########.fr       */
+/*   Updated: 2017/12/12 11:21:58 by ddenkin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	apply_plusspace(char **res, t_form *form, char symb)
 	char	*tmp;
 	char	*tmp2;
 
-	if (((*res)[0] == '0' || (*res)[0] == ' ') && ft_isdigit((*res)[1]))
+	if (((*res)[0] == '0') && ft_isdigit((*res)[1]))
 	{
 		if (ft_strlen(*res) > (size_t)form->prec && form->prec != -1)
 			(*res)[0] = symb;
