f2fc961ee99ad41e769a34a817dad709c1248954
Denys DENKIN <ddenkin@e2r11p9.unit.ua>
2017-12-15 18:31:33 +0200

    pls

--- ft_opp.h
+++ ft_opp.h
@@ -6,7 +6,7 @@
 /*   By: ddenkin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 21:34:07 by ddenkin           #+#    #+#             */
-/*   Updated: 2017/12/15 18:30:36 by ddenkin          ###   ########.fr       */
+/*   Updated: 2017/12/15 18:31:30 by ddenkin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ t_opp g_opptab[] =
 	{"c", &char_handler},
 	{"C", &wchar_handler},
 	{"%", &perc_handler},
-	{"e", &e_handler},
-	{"E", &e_handler},
+//	{"e", &e_handler},
+//	{"E", &e_handler},
 	{"f", &f_handler},
 	{"F", &f_handler},
 	{"b", &binary_handler},
