fce8990bf64e5a0dd5e1d57239dc8d950a484115
Denys DENKIN <ddenkin@e2r11p9.unit.ua>
2017-12-17 21:59:22 +0200

    date time fix

--- handlers/date_handler.c
+++ handlers/date_handler.c
@@ -6,22 +6,60 @@
 /*   By: ddenkin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/13 20:08:16 by ddenkin           #+#    #+#             */
-/*   Updated: 2017/12/13 20:26:48 by ddenkin          ###   ########.fr       */
+/*   Updated: 2017/12/17 21:56:45 by ddenkin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 #include <time.h>
 
-int		date_handler(va_list *va, t_form *form)
+static char	*date_str(struct tm tm)
+{
+	char	*res;
+	int		i;
+	int		temp;
+
+	i = 0;
+	res = (char *)malloc(sizeof(char) * 11);
+	temp = tm.tm_year + 1900;
+	while (i < 4)
+	{
+		res[3 - i] = temp % 10 + 48;
+		temp /= 10;
+		i++;
+	}
+	res[4] = '-';
+	temp = tm.tm_mon + 1;
+	res[5] = temp / 10 + 48;
+	res[6] = temp % 10 + 48;
+	res[7] = '-';
+	temp = tm.tm_mday;
+	res[8] = temp / 10 + 48;
+	res[9] = temp % 10 + 48;
+	res[10] = 0;
+	return (res);
+}
+
+int			date_handler(va_list *va, t_form *form)
 {
 	time_t		t;
+	int			ret;
 	struct tm	tm;
+	char		*res;
 
 	(void)form;
 	(void)va;
 	t = time(NULL);
 	tm = *localtime(&t);
-	return (ft_printf("%.4d-%.2d-%.2d", tm.tm_year + 1900,\
-				tm.tm_mon + 1, tm.tm_mday));
+	res = date_str(tm);
+	if (form->flg->minus == 1)
+		apply_blanks(&res, form, 1);
+	else if (form->flg->zero == 1 && form->prec == -1)
+		apply_zero(&res, form);
+	else
+		apply_blanks(&res, form, 0);
+	ft_putstr(res);
+	ret = ft_strlen(res);
+	ft_strdel(&res);
+	return (ret);
 }
--- handlers/time_handler.c
+++ handlers/time_handler.c
@@ -6,21 +6,56 @@
 /*   By: ddenkin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/13 20:22:50 by ddenkin           #+#    #+#             */
-/*   Updated: 2017/12/13 20:26:30 by ddenkin          ###   ########.fr       */
+/*   Updated: 2017/12/17 21:56:36 by ddenkin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 #include <time.h>
 
-int		time_handler(va_list *va, t_form *form)
+static char	*time_str(struct tm tm)
+{
+	char	*res;
+	int		i;
+	int		temp;
+
+	i = 0;
+	res = (char *)malloc(sizeof(char) * 9);
+	temp = tm.tm_hour;
+	res[0] = temp / 10 + 48;
+	res[1] = temp % 10 + 48;
+	res[2] = ':';
+	temp = tm.tm_min;
+	res[3] = temp / 10 + 48;
+	res[4] = temp % 10 + 48;
+	res[5] = ':';
+	temp = tm.tm_sec;
+	res[6] = temp / 10 + 48;
+	res[7] = temp % 10 + 48;
+	res[8] = 0;
+	return (res);
+}
+
+int			time_handler(va_list *va, t_form *form)
 {
 	time_t		t;
 	struct tm	tm;
+	char		*res;
+	int			ret;
 
 	(void)form;
 	(void)va;
 	t = time(NULL);
 	tm = *localtime(&t);
-	return (ft_printf("%.2d:%.2d:%.2d", tm.tm_hour, tm.tm_min, tm.tm_sec));
+	res = time_str(tm);
+	if (form->flg->minus == 1)
+		apply_blanks(&res, form, 1);
+	else if (form->flg->zero == 1 && form->prec == -1)
+		apply_zero(&res, form);
+	else
+		apply_blanks(&res, form, 0);
+	ft_putstr(res);
+	ret = ft_strlen(res);
+	ft_strdel(&res);
+	return (ret);
 }
